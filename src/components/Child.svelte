478cd013ac355858a020a3eeca0369dfbce160cc
anangkf_ <gonanggoneng@gmail.com>
2023-01-06 15:56:35 +0700

    nested components

---
+++ src/components/Child.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let data
+</script>
+
+<h2>This is child components!</h2>
+<p>I got data from props : "{data}"</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Child from '../components/Child.svelte'
   let name='world'
   let randomImg = 'https://scontent.fjog9-1.fna.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEWVbxRvnLB9g7w89jhvl2QIUR2JKXD5MAhRHYkpcPkwFX5syMpMwgZkvRlOQai4-TJLB1XMO759TMCyHrttnUa&_nc_ohc=PPDb9BF8iYAAX97DeqF&_nc_ht=scontent.fjog9-1.fna&oh=00_AfCHvvmD_GbMp_Gi6bEDllgFWOUnPL1M9CocDJAuPMmb2w&oe=63DF3A4E'
 </script>
@@ -6,6 +7,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Hello {name}!</p>
 <img class="image" src={randomImg} aria-hidden="true" alt="random-image">
+<Child data={'lorem ipsum dolor sit amet'}/>
 
 <style>
   .image{
