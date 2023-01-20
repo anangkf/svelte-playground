b89ff0dc61e7e9ec43637696607ffb4f37fed695
anangkf_ <gonanggoneng@gmail.com>
2023-01-20 15:50:55 +0700

    logic: if blocks

---
+++ src/routes/logic/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  let number;
+</script>
+
+<svelte:head>
+  <title>Logic</title>
+</svelte:head>
+
+<section>
+  <input type="number" bind:value={number}>
+  <p>Angka yang Anda masukkan adalah bilangan
+    {#if number % 2 === 0}
+      genap
+    {:else}
+      ganjil
+    {/if}
+  </p>
+</section>
