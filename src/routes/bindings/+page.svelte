3559c4b7764a07d89fbddff16f9defb2971fb169
anangkf_ <gonanggoneng@gmail.com>
2023-01-26 23:58:20 +0700

    bindings: numeric inputs

--- src/routes/bindings/+page.svelte
+++ src/routes/bindings/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   let name = '';
+  let age = '';
 </script>
 
 <svelte:head>
@@ -13,11 +14,16 @@
       <label for="name">
         Name: 
         <input type="text" bind:value={name} name="name">
+      </label><br>
+      <label for="age">
+        Age: 
+        <input type="number" bind:value={age} name="age">
       </label>
     </form>
     <aside class="content">
       <h2>Your data</h2>
       <p>Name: {name}</p>
+      <p>Age: {age}</p>
     </aside>
   </section>
 </main>
@@ -31,5 +37,9 @@
   .content{
     width: 40%;
     margin: 0 1rem;
+  }
+  label{
+    display: grid;
+    grid-template-columns: 1fr 3fr;
   }
 </style>
