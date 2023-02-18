0c5546baa944bbff3789e6202cae561d7d498517
anangkf_ <gonanggoneng@gmail.com>
2023-02-18 21:16:36 +0700

    bindings: component bindings

---
+++ src/routes/bindings/create-pin/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Keypad from '../../../components/Keypad.svelte';
+
+	let pin;
+	$: view = pin ? pin.replace(/\d(?!$)/g, '•') : 'enter your pin';
+  
+	function handleSubmit() {
+		alert(`submitted ${pin}`);
+	}
+  
+</script>
+
+<svelte:head>
+  <title>Create Pin</title>
+</svelte:head>
+
+<main>
+  <h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
+  <small>maximum 6 characters</small>
+  
+  <Keypad bind:value={pin} on:submit={handleSubmit}/>
+</main>
